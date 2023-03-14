4a3a3569bfd97a075eb04d048fed87a78c61f181
Weslei França <weslwifranca@gmail.com>
2023-03-14 18:03:13 -0300

    update styled.d.ts

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,12 +1,5 @@
 import 'styled-components'
 
-// import { light } from '../styles/theme/light'
-// import { dark } from '../styles/theme/dark'
-
-// const colors = light && dark
-
-// type ThemeType = typeof colors
-
 declare module 'styled-components' {
    export interface DefaultTheme {
       title: string
